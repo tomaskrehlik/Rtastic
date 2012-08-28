5da20326ca6a387937e9fce4cb4337b59265da4a
Tomáš Křehlík <tomas.krehlik@gmail.com>
2012-08-28 22:49:22 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rtastic::Application.config.secret_token = '3a7593f7c34f45d83bb3759a8e38890d0a177fdcc3ebb097d46a3e88d772c89870d5cdc9e93ed7fc69b31c6e4e45c272eaa6cb25d0049c5c67f8720a894b2676'
