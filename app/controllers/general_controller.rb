aa0d3e88fbe4451ce64506095e51bb247e01e994
Tomáš Křehlík <tomas.krehlik@gmail.com>
2012-10-02 18:22:06 +0200

    Cleaning up the routes config file

--- app/controllers/general_controller.rb
+++ app/controllers/general_controller.rb
@@ -6,4 +6,24 @@ class GeneralController < ApplicationController
       redirect_to "/packages/#{@packages[0].to_s}/"
     end
   end
+
+  def about
+
+  end
+
+  def development_plans
+  
+  end
+
+  def donate
+
+  end
+
+  def feedback
+
+  end
+
+  def home
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,81 +1,32 @@
 Rtastic::Application.routes.draw do
-  #get "users/new"
+  
+  # Root set up
+  root to: 'general#home'
+  match "/search", to: 'general#search'
+
+  # Users resources and routes
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
 
   match '/signup',  to: 'users#new'
   match '/signin',  to: 'sessions#new'
   match '/signout', to: 'sessions#destroy', via: :delete
-
-#original = tomas
+  
+  # Paintgraph routes
   match "/paintgraph", to: 'graph#paintgraph'
   match "/settingsgraph", to: 'graph#settingsgraph'
+
+  # Packages routes and admin routes
   match "/overview", to: 'packages#overview'
-  root to: 'packages#actions'
-  match "/packages/:id", to: 'packages#show', :constraints => { :id => /[0-z\.]+/ }
+  match "/packages/:name", to: 'packages#show', :constraints => { :name => /[0-z\.]+/ }
   match "/log", to: 'packages#log'
   match "/init", to: 'packages#init'
   match "/update", to: 'packages#update'
+
+  #Documentation routes the constraints are there because package and function names contain dots and hyphens
+  #which are not allowed by default
   match "/docs/:name", to: 'packages#documentation', :constraints => { :name => /[0-z\.-]+/ }
   match "/docs/:name/:function", to: 'packages#documentation', :constraints => { :name => /[0-z\.-]+/ , :function => /[0-z\.-]+/}
   match "/docsbuild/:name", to: 'packages#builddocs', :constraints => { :name => /[0-z\.-]+/ }
-#  match "/docs/:name/:function", to: 'packages#documentation'
 
-  match "/search", to: 'general#search'
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-    
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', :on => :collection
-  #     end
-  #   end
-
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
 end
